d4c496877b4fc99ce5154d0d748c4ec3881c7e0d
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-27 01:37:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   goats = ["jordan", "messi", "federer", "brady"]
-  goats << "bonds"
+  goats.push ("bonds")
 end
 
 def add_element_to_start_of_array(array, element)
