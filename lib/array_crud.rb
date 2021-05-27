e102f9ddae2392b90ac3b83478820219199f905e
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-27 01:22:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+ []
 end
 
 def create_an_array
-  
+  goats = ["jordan", "messi", "federer", "brady"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   goats << "bonds"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  goats.unshift ("gretzky")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  brady = goats.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  jordan = goats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  goats [2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  goats[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  goats[3]
 end
 
 def update_element_from_index(array, index_number, element)
-
+ 
 end
