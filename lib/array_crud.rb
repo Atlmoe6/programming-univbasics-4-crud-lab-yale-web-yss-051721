65c0f48c20472fc2f873977b76aad74c3f6b43c6
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-27 01:48:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   goats = ["jordan", "messi", "federer", "arrays!"]
-  "arrays!"
+  arrays! = goats.
 end
 
 def remove_element_from_start_of_array(array)
